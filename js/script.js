ba1585b159d86aad5dc67cd9b3be244cfe737c03
naufalalief <naufalalief087@gmail.com>
2023-09-17 00:17:55 +0700

    updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//
 const NavbarLinks = document.querySelector(".nav-links");
 
 document.querySelector("#burgir").onclick = () => {
@@ -11,7 +12,42 @@ document.addEventListener("click", function (e) {
     NavbarLinks.classList.remove("active");
   }
 });
+//
+function scrollHeader() {
+  const navbar = document.getElementById("navbar");
+  if (this.scrollY >= 150) navbar.classList.add("scroll-header");
+  else navbar.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+//
+const sections = document.querySelectorAll("section[id]");
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 100;
+    const sectionId = current.getAttribute("id");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      const activeLink = document.querySelector(
+        ".navbar .nav-links a[href*=" + sectionId + "]"
+      );
+      activeLink.classList.add("active-link");
+      activeLink.style.backgroundColor = "white";
+      activeLink.style.color = "transparent";
+      activeLink.style.WebkitBackgroundClip = "text";
+      activeLink.style.backgroundClip = "text";
+    } else {
+      const inactiveLink = document.querySelector(
+        ".navbar .nav-links a[href*=" + sectionId + "]"
+      );
+      inactiveLink.style.backgroundColor = "";
+      inactiveLink.style.color = "var(--primary)";
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+//
 window.addEventListener("scroll", reveal);
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
@@ -27,3 +63,40 @@ function reveal() {
     }
   }
 }
+//
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+function contactNotification() {
+  const x = document.getElementById("notification");
+  const name = document.querySelector('input[name="name"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+  const message = document.querySelector('input[name="message"]').value;
+
+  if (name.trim() !== "" && email.trim() !== "" && message.trim() !== "") {
+    x.className = "show";
+    setTimeout(() => {
+      x.className = x.className.replace("show", "");
+    }, 3000);
+  }
+}
+
+const btn = document.getElementById("btnSnack");
+btn.addEventListener("click", contactNotification);
+//
+document.getElementById("regis").addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+function regisNotification() {
+  const y = document.getElementById("register-notification");
+  const email = document.querySelector('input[name="register-email"]').value;
+  if (email.trim() !== "") {
+    y.className = "show";
+    setTimeout(() => {
+      y.className = y.className.replace("show", "");
+    }, 3000);
+  }
+}
+const reg = document.getElementById("regisSnack");
+reg.addEventListener("click", regisNotification);
+// 
